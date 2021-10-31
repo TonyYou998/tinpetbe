f58fb661c43383ac637f395ed21433e7b70630a9
TonyYou998 <66458772+TonyYou998@users.noreply.github.com>
2021-11-01 02:22:27 +0700

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns=[
+    path('',views.index,name='index'),
+    path('register',views.register,name='register'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('find',views.find,name="find"),
+    path('breed',views.breed,name='breed'),
+    path('give',views.give,name='give')
+
+]
---
+++ static/js/style.js
@@ -0,0 +1,10 @@
+
+$('.your-class').slick({
+    dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1
+  });
+
+  
