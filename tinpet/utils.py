af6d290619529c084cd041e26e4fe6dfa26bdb6d
TonyYou998 <66458772+TonyYou998@users.noreply.github.com>
2021-12-05 19:02:45 +0700

    push

--- tinpet/utils.py
+++ tinpet/utils.py
@@ -1,6 +1,6 @@
 from twilio.rest  import Client
 account_sid = 'AC96f5105aff90abe7ce797c607ef80e24'
-auth_token = '7c95d518d085d309d71128a4e03a815c'
+auth_token = '350f43c3518181222ba71e73ff0192c0'
 client = Client(account_sid, auth_token)
 def send_sms(user_code,phone_number):
 
